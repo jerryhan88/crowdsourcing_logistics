8c85f936aa68cad52905554db09dd67dc49d43a0
ckhan <chungkyun.han@gmail.com>
2017-08-01 16:36:22 +0800

    temp upload

--- experiments.py
+++ experiments.py
@@ -88,7 +88,7 @@ def local_run():
     points, travel_time, \
     flows, paths, \
     tasks, rewards, volumes, \
-    numBundles, thVolume, thDetour = ex8()
+    numBundles, thVolume, thDetour = ex9()
     inputs = [points, travel_time,
                 flows, paths,
                 tasks, rewards, volumes,
