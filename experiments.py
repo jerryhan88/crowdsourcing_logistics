8451fc59966c9a514e2d6edd19c82d809d299853
ckhan <chungkyun.han@gmail.com>
2017-08-01 13:51:15 +0800

    temp upload

--- experiments.py
+++ experiments.py
@@ -8,53 +8,43 @@ import csv
 import pickle
 
 def run(processorID, num_workers=11):
-    maxFlow = 3
-    minReward, maxReward = 1, 3
-    minVolume, maxVolume = 1, 2
-    thVolume = 3
-    detourAlowProp = 0.5
-    jobID = 0
-    for i in range(2, 9):
-        numCols = numRows = i
-        for numTasks in range(5, 50, 5):
-            for j in range(1, 10):
-                bundleResidualProp = 1 + j / 10.0
-                fn = 'nt%d-np%d-nb%d-tv%d-td%d.pkl' % (numTasks, numPaths, numBundles, thVolume, thDetour)
-                ifpath = opath.join(dpath['problem'], fn)
-                inputs = None
-                with open(ifpath, 'rb') as fp:
-                    inputs = pickle.load(fp)
-                if jobID % num_workers != processorID:
-                    continue
-                points, travel_time, \
-                flows, paths, \
-                tasks, rewards, volumes, \
-                numBundles, thVolume, thDetour = inputs
-                numTasks, numPaths = map(len, [tasks, paths])
-                fn = 'nt%d-np%d-nb%d-tv%d-td%d.csv' % (numTasks, numPaths, numBundles, thVolume, thDetour)
-                ofpath = opath.join(dpath['experiment'], fn)
-                with open(ofpath, 'wt') as w_csvfile:
-                    writer = csv.writer(w_csvfile, lineterminator='\n')
-                    header = ['numTasks', 'numPaths', 'numBundles', 'thVolume', 'thDetour',
-                              'm_obj', 'h_obj', 'gap_obj', 'm_time', 'h_time', 'gap_time', ]
-                    writer.writerow(header)
-                try:
-                    m_obj, m_time = run_mip_eliSubTour(convert_input4MathematicalModel(*inputs))
-                    h_obj, h_time = run_greedyHeuristic(convert_input4greedyHeuristic(*inputs))
-                    gap_obj = (m_obj - h_obj) / float(m_obj)
-                    gap_time = (m_time - h_time) / float(m_time)
-                    with open(ofpath, 'a') as w_csvfile:
-                        writer = csv.writer(w_csvfile, lineterminator='\n')
-                        writer.writerow([numTasks, numPaths, numBundles, thVolume, thDetour,
-                                         m_obj, h_obj, gap_obj,
-                                         m_time, h_time, gap_time])
-                except:
-                    with open(ofpath, 'a') as w_csvfile:
-                        writer = csv.writer(w_csvfile, lineterminator='\n')
-                        writer.writerow([numTasks, numPaths, numBundles, thVolume, thDetour,
-                                         -1, -1, -1,
-                                         -1, -1, -1])
-                jobID += 1
+    for i, fn in enumerate(os.listdir(dpath['problem'])):
+        if not fn.endswith('.pkl'):
+            continue
+        ifpath = opath.join(dpath['problem'], fn)
+        inputs = None
+        with open(ifpath, 'rb') as fp:
+            inputs = pickle.load(fp)
+        if i % num_workers != processorID:
+            continue
+        points, travel_time, \
+        flows, paths, \
+        tasks, rewards, volumes, \
+        numBundles, thVolume, thDetour = inputs
+        numTasks, numPaths = map(len, [tasks, paths])
+        fn = 'nt%d-np%d-nb%d-tv%d-td%d.csv' % (numTasks, numPaths, numBundles, thVolume, thDetour)
+        ofpath = opath.join(dpath['experiment'], fn)
+        with open(ofpath, 'wt') as w_csvfile:
+            writer = csv.writer(w_csvfile, lineterminator='\n')
+            header = ['numTasks', 'numPaths', 'numBundles', 'thVolume', 'thDetour',
+                      'm_obj', 'h_obj', 'gap_obj', 'm_time', 'h_time', 'gap_time', ]
+            writer.writerow(header)
+        try:
+            m_obj, m_time = run_mip_eliSubTour(convert_input4MathematicalModel(*inputs))
+            h_obj, h_time = run_greedyHeuristic(convert_input4greedyHeuristic(*inputs))
+            gap_obj = (m_obj - h_obj) / float(m_obj)
+            gap_time = (m_time - h_time) / float(m_time)
+            with open(ofpath, 'a') as w_csvfile:
+                writer = csv.writer(w_csvfile, lineterminator='\n')
+                writer.writerow([numTasks, numPaths, numBundles, thVolume, thDetour,
+                                 m_obj, h_obj, gap_obj,
+                                 m_time, h_time, gap_time])
+        except:
+            with open(ofpath, 'a') as w_csvfile:
+                writer = csv.writer(w_csvfile, lineterminator='\n')
+                writer.writerow([numTasks, numPaths, numBundles, thVolume, thDetour,
+                                 -1, -1, -1,
+                                 -1, -1, -1])
 
 
 if __name__ == '__main__':
