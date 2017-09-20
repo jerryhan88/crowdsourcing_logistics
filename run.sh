5a4e341b23d3eb13cc1c5a4f69d1a4f118d7ce0a
ckhan <chungkyun.han@gmail.com>
2017-09-20 13:22:09 +0800

    temp upload

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python -c "from experiments import server_run; server_run()" &
+python3 -c "from experiments import server_run; server_run()" &
 
 #for i in {0..6}; do
 #    python -c "from experiments import run; run($i, 7)" &
